8c9439675133d4f376136c1981b0b2454870697b
eldstal <laeder.keps@gmail.com>
2025-02-16 21:21:07 +0100

    Remove extraneous color schemes

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,22 +3,22 @@ return {
     { "farmergreg/vim-lastplace" },
 
     -- Colorschemes
-    { "phha/zenburn.nvim" },
-    { "rebelot/kanagawa.nvim" },
+    --{ "phha/zenburn.nvim" },
+    --{ "rebelot/kanagawa.nvim" },
     { "savq/melange-nvim" },
-    { "xero/miasma.nvim" },
-    { "chama-chomo/grail" },
+    --{ "xero/miasma.nvim" },
+    --{ "chama-chomo/grail" },
 
-    {
-        "zenbones-theme/zenbones.nvim",
-        dependencies = "rktjmp/lush.nvim",
-        lazy = false,
-        priority = 1000,
-        -- you can set set configuration options here
-        config = function()
-             --vim.g.zenbones_darken_comments = 45
-             --vim.cmd.colorscheme('zenburned')
-        end
-    },
+    --{
+    --    "zenbones-theme/zenbones.nvim",
+    --    dependencies = "rktjmp/lush.nvim",
+    --    lazy = false,
+    --    priority = 1000,
+    --    -- you can set set configuration options here
+    --    config = function()
+    --         --vim.g.zenbones_darken_comments = 45
+    --         --vim.cmd.colorscheme('zenburned')
+    --    end
+    --},
 
 }
